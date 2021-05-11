8eff12b3bdcf61e9b8c848d2fd5be932b56515fc
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-05-11 10:52:54 -0700

    destroy out of bound animals

--- Prototype 2/Assets/Scripts/DestroyOutOfBound.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBound.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
-    private float foodBound = 30.0f;
+    private float topBound = 30.0f;
+    private float lowBound = -10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,11 @@ public class DestroyOutOfBound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > foodBound)
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowBound)
         {
             Destroy(gameObject);
         }
