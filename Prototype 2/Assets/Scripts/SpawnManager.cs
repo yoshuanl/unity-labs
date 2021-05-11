6b14fd9d9f4ff95b3f9ebe8de01af8ec57b64d6c
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-05-11 13:00:13 -0700

    use InvokeRepeating to auto generate animals

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,22 +8,25 @@ public class SpawnManager : MonoBehaviour
     private float spawnXRange = 20.0f;
     private float spawnZPos = 20.0f;
 
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    void SpawnRandomAnimal()
+    {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnXRange, spawnXRange), 0, spawnZPos);
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
-
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
